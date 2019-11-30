23f67e764bd069f1ff0c423b2bf34cc00fdb7cca
nathanah <nathanahoffman@gmail.com>
2019-11-29 23:49:40 -0800

    Update maxTweeter.c

--- maxTweeter.c
+++ maxTweeter.c
@@ -3,7 +3,22 @@
 #include<stdlib.h>
 
 
+struct countNode{
+  string name;
+  int count;
+}
+
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
+  if(argc == 0){
+    perror("No file given\n");
+  }
+  else{
 
-
+  }
 }
